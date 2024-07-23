a87fc652242605902c4320182692ec34383efc51
cewno <my@cewno.top>
2024-07-23 09:59:26 +0800

    修正 84f951f8

--- HPACKTool/IntegerToolDecoder.cs
+++ HPACKTool/IntegerToolDecoder.cs
@@ -4,7 +4,6 @@ namespace HPACKTool;
 
 public static class IntegerTool
 {
-	private static readonly byte[] Nb = {0b_0,0b_00000001,0b_00000011,0b_00000111,0b_00001111,0b_00011111,0b_00111111,0b_01111111,0b_11111111};
 	private static readonly byte[] Nb = new byte[]
 	{
 		0b_0,        //index 0
@@ -20,6 +19,13 @@ public static class IntegerTool
 
 	#region rUInt
 	
+	/// <summary>
+	/// 从双线程异步io中读取数字并解码
+	/// ！！！方法不提供大小检查，请确保使用的返回类型的最大值大于所设定最大值
+	/// </summary>
+	/// <param name="n">前缀长度,参见 <a href="https://www.rfc-editor.org/rfc/rfc7541.html#section-5.1/">RFC7541 第5.1节</a></param>
+	/// <param name="asyncIo">异步io</param>
+	/// <returns>解码后的数字</returns>
 	public static uint ReadUInt(byte n, AsyncIO asyncIo)
 	{
 		byte at = (byte)(asyncIo.ReadOneByte() & Nb[n]);
@@ -81,9 +87,45 @@ public static class IntegerTool
 			return (byte)at;
 		}
 	}
+
+	public static uint ReadUInt(byte n, byte[] buffer, int offset = 0)
+	{
+		int index = offset;
+		byte at = (byte)(buffer[index++] & Nb[n]);
+		//	比如 n = 5 时 下面这种情况会进入if
+		//	  0   1   2   3   4   5   6   7
+		//	+---+---+---+---+---+---+---+---+
+		//	| ? | ? | ? | 1   1   1   1   1 |
+		//	+---+---+---+-------------------+
+		if ((Nb[n] & at) == Nb[n])
+		{
+			byte m = 0;
+			uint i = 0;
+			do
+			{
+				at = buffer[index++];
+				i |= (uint)((at & 0b_01111111) << m);
+				m += 7;
+			} while ((at & 0b_10000000) == 0b_10000000);
+
+			return i + Nb[n];
+		}
+		else
+		{
+			return at;
+		}
+	}
 	#endregion
 
 	#region rULong
+
+	/// <summary>
+	/// 从双线程异步io中读取数字并解码
+	/// ！！！方法不提供大小检查，请确保使用的返回类型的最大值大于所设定最大值
+	/// </summary>
+	/// <param name="n">前缀长度,参见 <a href="https://www.rfc-editor.org/rfc/rfc7541.html#section-5.1/">RFC7541 第5.1节</a></param>
+	/// <param name="asyncIo">异步io</param>
+	/// <returns>解码后的数字</returns>
 	public static ulong ReadULong(byte n, AsyncIO asyncIo)
 	{
 		byte at = (byte)(asyncIo.ReadOneByte() & Nb[n]);
@@ -144,9 +186,45 @@ public static class IntegerTool
 			return (byte)at;
 		}
 	}
+	
+	public static ulong ReadULong(byte n, byte[] buffer, int offset = 0)
+	{
+		int index = offset;
+		byte at = (byte)(buffer[index++] & Nb[n]);
+		//	比如 n = 5 时 下面这种情况会进入if
+		//	  0   1   2   3   4   5   6   7
+		//	+---+---+---+---+---+---+---+---+
+		//	| ? | ? | ? | 1   1   1   1   1 |
+		//	+---+---+---+-------------------+
+		if ((Nb[n] & at) == Nb[n])
+		{
+			ushort m = 0;
+			ulong i = 0;
+			do
+			{
+				at = buffer[index++];
+				i |= (ulong)((at & 0b_01111111) << m);
+				m += 7;
+			} while ((at & 0b_10000000) == 0b_10000000);
+
+			return i + Nb[n];
+		}
+		else
+		{
+			return at;
+		}
+	}
 	#endregion
 	
 	#region rUShort
+	
+	/// <summary>
+	/// 从双线程异步io中读取数字并解码
+	/// ！！！方法不提供大小检查，请确保使用的返回类型的最大值大于所设定最大值
+	/// </summary>
+	/// <param name="n">前缀长度,参见 <a href="https://www.rfc-editor.org/rfc/rfc7541.html#section-5.1/">RFC7541 第5.1节</a></param>
+	/// <param name="asyncIo">异步io</param>
+	/// <returns>解码后的数字</returns>
 	public static ushort ReadUShort(byte n, AsyncIO asyncIo)
 	{
 		byte at = (byte)(asyncIo.ReadOneByte() & Nb[n]);
@@ -207,9 +285,45 @@ public static class IntegerTool
 			return (byte)at;
 		}
 	}
+	public static ushort ReadUShort(byte n, byte[] buffer, int offset = 0)
+	{
+		int index = offset;
+		byte at = (byte)(buffer[index++] & Nb[n]);
+		//	比如 n = 5 时 下面这种情况会进入if
+		//	  0   1   2   3   4   5   6   7
+		//	+---+---+---+---+---+---+---+---+
+		//	| ? | ? | ? | 1   1   1   1   1 |
+		//	+---+---+---+-------------------+
+		if ((Nb[n] & at) == Nb[n])
+		{
+			byte m = 0;
+			ushort i = 0;
+			do
+			{
+				at = buffer[index++];
+				i |= (ushort)((at & 0b_01111111) << m);
+				m += 7;
+			} while ((at & 0b_10000000) == 0b_10000000);
+
+			return (ushort)(i + Nb[n]);
+		}
+		else
+		{
+			return at;
+		}
+	}
+	
 	#endregion
 
 	#region rByte
+
+	/// <summary>
+	/// 从双线程异步io中读取数字并解码
+	/// ！！！方法不提供大小检查，请确保使用的返回类型的最大值大于所设定最大值
+	/// </summary>
+	/// <param name="n">前缀长度,参见 <a href="https://www.rfc-editor.org/rfc/rfc7541.html#section-5.1/">RFC7541 第5.1节</a></param>
+	/// <param name="asyncIo">异步io</param>
+	/// <returns>解码后的数字</returns>
 	public static byte ReadByte(byte n, AsyncIO asyncIo)
 	{
 		byte at = (byte)(asyncIo.ReadOneByte() & Nb[n]);
@@ -270,9 +384,39 @@ public static class IntegerTool
 			return (byte)at;
 		}
 	}
+	public static byte ReadByte(byte n, byte[] buffer, int offset = 0)
+	{
+		int index = offset;
+		byte at = (byte)(buffer[index++] & Nb[n]);
+		//	比如 n = 5 时 下面这种情况会进入if
+		//	  0   1   2   3   4   5   6   7
+		//	+---+---+---+---+---+---+---+---+
+		//	| ? | ? | ? | 1   1   1   1   1 |
+		//	+---+---+---+-------------------+
+		if (at == Nb[n])
+		{
+			byte m = 0;
+			byte i = 0;
+			do
+			{
+				at = buffer[index++];
+				i |= (byte)((at & 0b_01111111) << m);
+				m += 7;
+			} while ((at & 0b_10000000) == 0b_10000000);
+
+			return (byte)(i + Nb[n]);
+		}
+		else
+		{
+			return at;
+		}
+	}
+	
 	#endregion
 
 	#region RUint128
+	
+	
 #if NET7_0_OR_GREATER
 	public static UInt128 ReadUInt128(byte n, AsyncIO asyncIo)
 	{
@@ -335,11 +479,19 @@ public static class IntegerTool
 		}
 	}
 #endif
-
 	
 	#endregion
 
 	#region RBigint
+
+
+	/// <summary>
+	/// 从双线程异步io中读取数字并解码
+	/// ！！！方法不提供大小检查，请确保使用的返回类型的最大值大于所设定最大值
+	/// </summary>
+	/// <param name="n">前缀长度,参见 <a href="https://www.rfc-editor.org/rfc/rfc7541.html#section-5.1/">RFC7541 第5.1节</a></param>
+	/// <param name="asyncIo">异步io</param>
+	/// <returns>解码后的数字</returns>
 	public static BigInteger ReadBigInteger(byte n, AsyncIO asyncIo)
 	{
 		byte at = (byte)(asyncIo.ReadOneByte() & Nb[n]);
@@ -390,6 +542,34 @@ public static class IntegerTool
 					throw new IOException();
 				}
 				i |= (at & 0b_01111111) * (2^m);
+				m += 7;
+			} while ((at & 0b_10000000) == 0b_10000000);
+
+			return i + Nb[n];
+		}
+		else
+		{
+			return at;
+		}
+	}
+
+	public static BigInteger ReadBigInteger(byte n, byte[] buffer, int offset = 0)
+	{
+		int index = offset;
+		byte at = (byte)(buffer[index++] & Nb[n]);
+		//	比如 n = 5 时 下面这种情况会进入if
+		//	  0   1   2   3   4   5   6   7
+		//	+---+---+---+---+---+---+---+---+
+		//	| ? | ? | ? | 1   1   1   1   1 |
+		//	+---+---+---+-------------------+
+		if ((Nb[n] & at) == Nb[n])
+		{
+			BigInteger m = 0;
+			BigInteger i = 0;
+			do
+			{
+				at = buffer[index++];
+				i |= (at & 0b_01111111) * (2 ^ m);
 				m += 7;
 			} while ((at & 0b_10000000) == 0b_10000000);
 
