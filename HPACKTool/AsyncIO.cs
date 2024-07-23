d498e081441ad95ffc037c7963248e5b3b2e021c
cewno <my@cewno.top>
2024-07-23 17:38:16 +0800

    扩展异步io接口

--- HPACKTool/AsyncIO.cs
+++ HPACKTool/AsyncIO.cs
@@ -3,4 +3,8 @@ namespace HPACKTool;
 public interface AsyncIO
 {
 	public byte ReadOneByte();
+	public byte Read(byte[] buffer,int offset,int length);
+	public void writeOneByte(byte d);
+	public void write(byte[] buffer,int offset,int length);
+
 }
