b9c3b3061e9769a46114b63dfc1bfc2e4a6a4d5b
cewno <my@cewno.top>
2024-07-24 19:54:11 +0800

    添加整数解码编码工具测试类

---
+++ HPACKTool/Test/IntegerTest.cs
@@ -0,0 +1,381 @@
+#if DEBUG
+using Xunit;
+
+namespace HPACKTool.Test;
+
+public class IntegerTest
+{
+	private static readonly byte[] Nb =
+	{
+		0b_0,        //index 0
+		0b_00000001, //index 1
+		0b_00000011, //index 2
+		0b_00000111, //index 3
+		0b_00001111, //index 4
+		0b_00011111, //index 5
+		0b_00111111, //index 6
+		0b_01111111, //index 7
+		0b_11111111  //index 8
+	};
+	
+	[Fact]
+	public void test()
+	{
+		ushort cs = 40000;
+		for (byte i = 1; i <= 8; i++)
+		{
+			byte[] buffer = IntegerTool.WriteUInteger((uint)0,i,0);
+			TestReadIO testReadIo = new TestReadIO(buffer);
+				
+			if (IntegerTool.ReadUInt(i,(AsyncIO)testReadIo) != 0)
+			{
+				throw new Exception();
+			}
+		}
+		for (byte i = 1; i <= 8; i++)
+		{
+			for (ushort j = 0; j < cs; j++)
+			{
+				uint next = (uint)new Random().Next(int.MinValue + 1, int.MaxValue - 1);
+				byte[] buffer = IntegerTool.WriteUInteger(next,i,0);
+				TestReadIO testReadIo = new TestReadIO(buffer);
+				
+				if (IntegerTool.ReadUInt(i,(AsyncIO)testReadIo) != next)
+				{
+					throw new Exception();
+				}
+			}
+		}
+		
+		for (byte i = 1; i <= 8; i++)
+		{
+			byte[] buffer = IntegerTool.WriteUInteger((ulong)0,i,0);
+			TestReadIO testReadIo = new TestReadIO(buffer);
+				
+			if (IntegerTool.ReadULong(i,(AsyncIO)testReadIo) != 0)
+			{
+				throw new Exception();
+			}
+		}
+		for (byte i = 1; i <= 8; i++)
+		{
+			for (ushort j = 0; j < cs; j++)
+			{
+				ulong next = (ulong)new Random().NextInt64(long.MinValue + 1, long.MaxValue - 1);
+				byte[] buffer = IntegerTool.WriteUInteger(next,i,0);
+				TestReadIO testReadIo = new TestReadIO(buffer);
+				
+				if (IntegerTool.ReadULong(i,(AsyncIO)testReadIo) != next)
+				{
+					throw new Exception();
+				}
+			}
+		}
+		for (byte i = 1; i <= 8; i++)
+		{
+			byte[] buffer = IntegerTool.WriteUInteger((ushort)0,i,0);
+			TestReadIO testReadIo = new TestReadIO(buffer);
+				
+			if (IntegerTool.ReadUShort(i,(AsyncIO)testReadIo) != 0)
+			{
+				throw new Exception();
+			}
+		}
+		for (byte i = 1; i <= 8; i++)
+		{
+			for (ushort j = 0; j < cs; j++)
+			{
+				ushort next = (ushort)new Random().Next(1, ushort.MaxValue - 1);
+				byte[] buffer = IntegerTool.WriteUInteger(next,i,0);
+				TestReadIO testReadIo = new TestReadIO(buffer);
+				
+				if (IntegerTool.ReadUShort(i,(AsyncIO)testReadIo) != next)
+				{
+					throw new Exception();
+				}
+			}
+		}
+		for (byte i = 1; i <= 8; i++)
+		{
+			byte[] buffer = IntegerTool.WriteUInteger((byte)0,i,0);
+			TestReadIO testReadIo = new TestReadIO(buffer);
+				
+			if (IntegerTool.ReadByte(i,(AsyncIO)testReadIo) != 0)
+			{
+				throw new Exception();
+			}
+		}
+		for (byte i = 1; i <= 8; i++)
+		{
+			for (byte j = 0; j < byte.MaxValue; j++)
+			{
+				byte[] buffer = IntegerTool.WriteUInteger(j,i,0);
+				TestReadIO testReadIo = new TestReadIO(buffer);
+				
+				if (IntegerTool.ReadByte(i,(AsyncIO)testReadIo) != j)
+				{
+					throw new Exception();
+				}
+			}
+		}
+#if NET7_0_OR_GREATER
+		for (byte i = 1; i <= 8; i++)
+		{
+			byte[] buffer = IntegerTool.WriteUInteger((UInt128)0,i,0);
+			TestReadIO testReadIo = new TestReadIO(buffer);
+				
+			if (IntegerTool.ReadUInt128(i,(AsyncIO)testReadIo) != 0)
+			{
+				throw new Exception();
+			}
+		}
+		for (byte i = 1; i <= 8; i++)
+		{
+			for (ushort j = 0; j < cs; j++)
+			{
+				UInt128 next = new UInt128((ulong)new Random().NextInt64(long.MinValue, long.MaxValue),
+											(ulong)new Random().NextInt64(long.MinValue + 1, long.MaxValue - 1));
+				byte[] buffer = IntegerTool.WriteUInteger(next,i,0);
+				TestReadIO testReadIo = new TestReadIO(buffer);
+				
+				if (IntegerTool.ReadUInt128(i,(AsyncIO)testReadIo) != next)
+				{
+					throw new Exception();
+				}
+			}
+		}	
+#endif
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		for (byte i = 1; i <= 8; i++)
+		{
+			byte[] buffer = IntegerTool.WriteUInteger(uint.MaxValue,i,0);
+			TestReadIO testReadIo = new TestReadIO(buffer);
+				
+			if (IntegerTool.ReadUInt(i,(AsyncIO)testReadIo) != uint.MaxValue)
+			{
+				throw new Exception();
+			}
+		}
+		
+		for (byte i = 1; i <= 8; i++)
+		{
+			byte[] buffer = IntegerTool.WriteUInteger(uint.MaxValue,i,0);
+			buffer[buffer.Length - 1] += 1;
+			TestReadIO testReadIo = new TestReadIO(buffer);
+
+			try
+			{
+				if (IntegerTool.ReadUInt(i,(AsyncIO)testReadIo) != uint.MaxValue)
+				{
+					throw new Exception();
+				}
+			}
+			catch (OverflowException e)
+			{
+				goto ok;
+			}
+
+			throw new Exception();
+			ok:
+			;
+
+		}
+		
+		for (byte i = 1; i <= 8; i++)
+		{
+			byte[] buffer = IntegerTool.WriteUInteger(ulong.MaxValue,i,0);
+			TestReadIO testReadIo = new TestReadIO(buffer);
+				
+			if (IntegerTool.ReadULong(i,(AsyncIO)testReadIo) != ulong.MaxValue)
+			{
+				throw new Exception();
+			}
+		}
+		
+		for (byte i = 1; i <= 8; i++)
+		{
+			byte[] buffer = IntegerTool.WriteUInteger(ulong.MaxValue,i,0);
+			buffer[buffer.Length - 1] += 1;
+			TestReadIO testReadIo = new TestReadIO(buffer);
+				
+			try
+			{
+				if (IntegerTool.ReadULong(i, (AsyncIO)testReadIo) != ulong.MaxValue)
+				{
+					throw new Exception();
+				}
+			}
+			catch (OverflowException e)
+			{
+				goto ok;
+			}
+
+			throw new Exception();
+			ok:
+			;
+			
+		}
+		for (byte i = 1; i <= 8; i++)
+		{
+			byte[] buffer = IntegerTool.WriteUInteger(byte.MaxValue,i,0);
+			TestReadIO testReadIo = new TestReadIO(buffer);
+				
+			if (IntegerTool.ReadByte(i,(AsyncIO)testReadIo) != byte.MaxValue)
+			{
+				throw new Exception();
+			}
+		}
+		for (byte i = 1; i <= 8; i++)
+		{
+			byte[] buffer = IntegerTool.WriteUInteger(byte.MaxValue,i,0);
+			buffer[buffer.Length - 1] += 1;
+			TestReadIO testReadIo = new TestReadIO(buffer);
+				
+			try
+			{
+				if (IntegerTool.ReadByte(i, (AsyncIO)testReadIo) != byte.MaxValue)
+				{
+					throw new Exception();
+				}
+			}
+			catch (OverflowException e)
+			{
+				goto ok;
+			}
+
+			throw new Exception();
+			ok:
+			;
+		}
+		for (byte i = 1; i <= 8; i++)
+		{
+			byte[] buffer = IntegerTool.WriteUInteger(ushort.MaxValue,i,0);
+			TestReadIO testReadIo = new TestReadIO(buffer);
+				
+			if (IntegerTool.ReadUShort(i,(AsyncIO)testReadIo) != ushort.MaxValue)
+			{
+				throw new Exception();
+			}
+		}
+		for (byte i = 1; i <= 8; i++)
+		{
+			byte[] buffer = IntegerTool.WriteUInteger(ushort.MaxValue,i,0);
+			buffer[buffer.Length - 1] += 1;
+			TestReadIO testReadIo = new TestReadIO(buffer);
+				
+			try
+			{
+				if (IntegerTool.ReadUShort(i, (AsyncIO)testReadIo) != ushort.MaxValue)
+				{
+					throw new Exception();
+				}
+			}
+			catch (OverflowException e)
+			{
+				goto ok;
+			}
+
+			throw new Exception();
+			ok:
+			;
+		}
+#if NET7_0_OR_GREATER
+		
+
+		for (byte i = 1; i <= 8; i++)
+		{
+			byte[] buffer = IntegerTool.WriteUInteger(UInt128.MaxValue,i,0);
+			TestReadIO testReadIo = new TestReadIO(buffer);
+				
+			if (IntegerTool.ReadUInt128(i,(AsyncIO)testReadIo) != UInt128.MaxValue)
+			{
+				throw new Exception();
+			}
+		}
+		for (byte i = 1; i <= 8; i++)
+		{
+			byte[] buffer = IntegerTool.WriteUInteger(UInt128.MaxValue,i,0);
+			buffer[buffer.Length - 1] += 1;
+			TestReadIO testReadIo = new TestReadIO(buffer);
+				
+			try
+			{
+				if (IntegerTool.ReadUInt128(i, (AsyncIO)testReadIo) != UInt128.MaxValue)
+				{
+					throw new Exception();
+				}
+			}
+			catch (OverflowException e)
+			{
+				goto ok;
+			}
+
+			throw new Exception();
+			ok:
+			;
+		}
+#endif
+	}
+}
+
+internal class TestReadIO : Stream, AsyncIO
+{
+	public override void Flush()
+	{
+		throw new NotImplementedException();
+	}
+
+	private byte[] buffer;
+	internal TestReadIO(byte[] buffer)
+	{
+		this.buffer = buffer;
+	}
+
+	private int i;
+	public byte ReadOneByte()
+	{
+		return buffer[i++];
+	}
+
+	public override int Read(byte[] buffer, int offset, int count)
+	{
+		throw new NotImplementedException();
+	}
+
+	public override long Seek(long offset, SeekOrigin origin)
+	{
+		throw new NotImplementedException();
+	}
+
+	public override void SetLength(long value)
+	{
+		throw new NotImplementedException();
+	}
+
+	public override void Write(byte[] buffer, int offset, int count)
+	{
+		throw new NotImplementedException();
+	}
+
+	public override bool CanRead { get; }
+	public override bool CanSeek { get; }
+	public override bool CanWrite { get; }
+	public override long Length { get; }
+	public override long Position { get; set; }
+}
+#endif
